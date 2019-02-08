a9f3bcff9e21d732c199d13208cfd7442c1ab4a2
kiuyas <kiuyas2@gmail.com>
2019-02-08 22:56:46 +0900

    1st commit

---
+++ FireWorks/FireWorks/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FireWorks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnFire = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(624, 441);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // btnFire
+            // 
+            this.btnFire.Location = new System.Drawing.Point(16, 8);
+            this.btnFire.Name = "btnFire";
+            this.btnFire.Size = new System.Drawing.Size(75, 23);
+            this.btnFire.TabIndex = 1;
+            this.btnFire.Text = "Fire!";
+            this.btnFire.UseVisualStyleBackColor = true;
+            this.btnFire.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.btnFire);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "FireWorks";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnFire;
+    }
+}
+
---
+++ FireWorks/FireWorks/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FireWorks
+{
+    public partial class Form1 : Form
+    {
+        private int count = 0;
+
+        private Spark[] _Sparks = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Init()
+        {
+            _Sparks = new Spark[8];
+            for (int i = 0; i < 8; i++)
+            {
+                _Sparks[i] = new Spark(new Point(320, 400));
+                _Sparks[i].V.Y = -8;
+            }
+            count = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnFire.Enabled = false;
+            Init();
+            timer1.Start();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (_Sparks == null)
+            {
+                return;
+            }
+            foreach (Spark s in _Sparks)
+            {
+                e.Graphics.FillEllipse(Brushes.White, s.P.X, s.P.Y, 8, 8);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (count < 220)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    _Sparks[i].Move();
+                }
+
+                if (count == 100)
+                {
+                    Explosion();
+                }
+            }
+            else if (count == 220)
+            {
+                timer1.Stop();
+                btnFire.Enabled = true;
+            }
+
+            count++;
+
+            pictureBox1.Refresh();
+        }
+
+        private void Explosion()
+        {
+            Console.WriteLine("Explosion!");
+            float v = 2;
+            for (int i = 0; i < 8; i++)
+            {
+                double radian = i * 45 * Math.PI / 180F;
+                _Sparks[i].V = new Point((float)Math.Cos(radian) * v, (float)Math.Sin(radian) * v);
+            }
+        }
+    }
+
+    class Spark
+    {
+        public Point P { get; set; }
+
+        public Point V { get; set; }
+
+        public Spark(Point p)
+        {
+            P = p;
+            V = new Point(0, 0);
+        }
+
+        public void Move()
+        {
+            P.X += V.X;
+            P.Y += V.Y;
+            V.Y += 0.1F;
+        }
+    }
+
+    class Point
+    {
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+}
